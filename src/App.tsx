6590fff2568ca3ae374843309e330ee711966e8e
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2023-05-20 01:48:25 -0300

    style: add tailwindcss to the form

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,29 @@ import './styles/global.css';
 
 export function App() {
   return (
-    <main>
-      <form>
-        <div>
+    <main className="h-screen bg-zinc-50 flex items-center justify-center">
+      <form className="flex flex-col gap-4">
+        <div className="flex flex-col gap-1">
           <label>E-mail</label>
           <input 
             type="email"
             name="email"
+            className="border border-zinc-200 shadow-sm rounded h-10" 
           />
         </div>
 
-        <div>
+        <div className="flex flex-col gap-1">
           <label>Password</label>
           <input 
             type="password"
             name="password"
+            className="border border-zinc-200 shadow-sm rounded h-10" 
           />
         </div>
 
         <button 
           type="submit"
+          className="bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600 transition"
         >
           Submit
         </button>     
