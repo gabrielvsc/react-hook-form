5640441d3692811265b8d3cb4a11f8550db5d14d
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2023-05-25 23:50:42 -0300

    feat: add transform & refine to inputs validation

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,18 @@ import { zodResolver } from '@hookform/resolvers/zod';
 // Forms Structure
 const createUserFormSchema = z.object({
   name: z.string()
-    .nonempty('O nome é obrigatório'),
+    .nonempty('O nome é obrigatório')
+    .transform(name => {
+      return name.trim().split(' ').map(word => {
+        return word[0].toLocaleUpperCase().concat(word.substring(1))
+      }).join(' ')
+    }),
   email: z.string()
     .nonempty('O e-mail é obrigatório')
-    .email('Formato de e-mail inválido'),
+    .email('Formato de e-mail inválido')
+    .refine(email => {
+      return email.endsWith('@rocketseat.com.br')
+    }, 'O e-mail precisa ser da Rocketseat'),
   password: z.string()
     .min(6, 'A senha preciso de no mínimo 6 caracteres'),
 })
